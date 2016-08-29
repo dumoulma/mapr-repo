d565ccc03b8ad948a7292e249fb69265aa0bee1f
mapr <mapr@mapr.com>
2016-08-29 09:03:00 +0000

    intiial commit

---
+++ drools-telecom-demo/src/main/java/com/mapr/qss/cep/demo/telecom/CallType.java
@@ -0,0 +1,5 @@
+package com.mapr.qss.cep.demo.telecom;
+
+public enum CallType {
+  Voice, SMS, DATA, SWITCH_ON, LOCATION_UPDATE;
+}
---
+++ drools-telecom-demo/src/main/java/com/mapr/qss/cep/demo/telecom/Coverage.java
@@ -0,0 +1,7 @@
+package com.mapr.qss.cep.demo.telecom;
+
+public enum Coverage {
+  SITE_2G,
+  SITE_3G,
+  SITE_4G;
+}
