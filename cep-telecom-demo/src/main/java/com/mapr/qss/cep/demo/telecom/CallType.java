c7ded36741e5a26fd6b6b8960a635b9080f27b12
cepuser <>
2016-08-29 08:54:12 +0000

    Module removed delete {/cep-telecom-demo}

--- cep-telecom-demo/src/main/java/com/mapr/qss/cep/demo/telecom/CallType.java
+++
@@ -1,5 +0,0 @@
-package com.mapr.qss.cep.demo.telecom;
-
-public enum CallType {
-  Voice, SMS, DATA, SWITCH_ON, LOCATION_UPDATE;
-}
--- cep-telecom-demo/src/main/java/com/mapr/qss/cep/demo/telecom/Coverage.java
+++
@@ -1,7 +0,0 @@
-package com.mapr.qss.cep.demo.telecom;
-
-public enum Coverage {
-  SITE_2G,
-  SITE_3G,
-  SITE_4G;
-}
